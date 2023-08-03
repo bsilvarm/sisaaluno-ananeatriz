70085450c03264600037dc8ac4df4037b69bd032
Ana Beatriz <130497945+bsilvarm@users.noreply.github.com>
2023-08-02 22:22:39 -0300

    Update index.php

--- sisaaluno/index.php
+++ sisaaluno/index.php
@@ -1,28 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="index.css">
-    <title>Document</title>
+  <title>Document</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <style>
+  
+  </style>
 </head>
 <body>
 
-    <div class="grid-container">
-  <div><div class="container">
-    <h1>CADASTRO ALUNO</h1>
-    <div class="botoes">
-   <button class="button"><a href="cadaluno.php">cadastrar</a></button>
-    <button class="button"><a href="listaalunos.php">lista</a></button>
+<div class="p-5 bg-dark text-white text-center">
+  <h1>Escola Virtual</h1>
+ 
+</div>
+
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2></h2>
+      <div class="fakeimg"><img src="escola.jpeg" width="415px" height="250px"></div>
+      <p>Sistema de Cadastro da Escola do Nunca
+      </p>
+      <h3 class="mt-4">Cadastro</h3>
+      <p>Aperta aqui para realizar cadastro!</p>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link "
+ href="cadaluno.php" >Cadastrar alunos
+  
+  
+</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="listaalunos.php">Lista de alunos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="cadprof.php">Cadastrar professores</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link " href="listaprof.php">Lista de professores</a>
+        </li>
+      </ul>
+      <hr class="d-sm-none">
     </div>
-    </div></div>
-  <div><div class="container2">
-    <h1>CADASTRO PROFESSOR</h1>
-    <div class="botoes2">
-    <button class="button"><a href="cadprof.php">cadastrar</a></button>
-    <button class="button"><a href="listaprof.php">lista</a></button>
+    <div class="col-sm-8">
+      <h2>   <li class="nav-item">
+          <a class="nav-link "
+ href="cadaluno.php" >Cadastrar alunos
+  
+  
+</a></h2>
+      <div class="fakeimg"> <img src="aluno.jpeg" width="900px" height="400px"></div>
+     
+
+      <h2 class="mt-5"><li class="nav-item">
+          <a class="nav-link" href="cadprof.php">Cadastrar professores</a>
+        </li></h2>
+   
+      <div class="fakeimg"><img src="prof.jpeg" width="900px" height="400px"></div>
+     
     </div>
-    </div></div>
+  </div>
+</div>
+
+<div class="mt-5 p-4 bg-dark text-white text-center">
+  <p>@bsilva.rm</p>
+</div>
+
 </body>
-</html>
+</html
