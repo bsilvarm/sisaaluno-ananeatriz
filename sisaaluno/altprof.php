ef3130f909e502b9b287b921a01aeaad604b4cde
Ana Beatriz <130497945+bsilvarm@users.noreply.github.com>
2023-08-04 15:53:26 -0300

    Update altprof.php

--- sisaaluno/altprof.php
+++ sisaaluno/altprof.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="altprof.css">
 </head>
 <body>
 
@@ -14,7 +15,7 @@
    $id = $_POST['id'];
 
    ##sql para selecionar apens um aluno
-   $sql = "SELECT * FROM aluno where id= :id";
+   $sql = "SELECT * FROM professor where id= :id";
    
    # junta o sql a conexao do banco
    $retorno = $conexao->prepare($sql);
@@ -33,24 +34,29 @@
    $endereco = $array_retorno['endereco'];
    $idade = $array_retorno['idade'];
    $datanascimento = $array_retorno['datanascimento'];
-   
-?>
 
-  <form method="POST" action="crudprof.php">
-        <label for="">nome</label>
+ 
+?>
+<div class="container">
+<h1>Cadastro de professores</h1>
+<h4>Alterar Dados</h4>
+<form method="POST" action="crudprof.php">
+        <label for="">Nome</label>
         <input type="text" name="nome" id="" value=<?php echo $nome?> required >
-        <label for="">endereco</label>                                        
+        <label for="">Endereco</label>                                        
         <input type="text" name="endereco" id="" value=<?php echo $endereco ?> required >
-        <label for="">idade</label>
+        <label for="">Idade</label>
         <input type="text" name="idade" id="" value=<?php echo $idade ?> required >
-        <label for="">data</label>
+        <label for="">Data de Nascimento</label>
         <input type="text" name="datanascimento" id="" value=<?php echo $datanascimento ?> required >
-        
-      
+
+    
       
         <input type="hidden" name="id" id="" value=<?php echo $id ?> >
         
         <input type="submit" name="update" value="Alterar">
   </form>
+</div>
+        
 </body>
 </html>
