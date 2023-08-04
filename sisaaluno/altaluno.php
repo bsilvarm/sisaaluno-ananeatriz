67fad1a3ff50be1e053e604c5e24b66c16a97d31
Ana Beatriz <130497945+bsilvarm@users.noreply.github.com>
2023-08-04 15:52:50 -0300

    Update altaluno.php

--- sisaaluno/altaluno.php
+++ sisaaluno/altaluno.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="altaluno.css">
 </head>
 <body>
 
@@ -36,23 +37,27 @@
 
    $matricula = $array_retorno['matricula'];
 ?>
-
-  <form method="POST" action="crudaluno.php">
-        <label for="">nome</label>
+<div class="container">
+<h1>Cadastro de alunos</h1>
+<h4>Alterar Dados</h4>
+<form method="POST" action="crudaluno.php">
+        <label for="">Nome</label>
         <input type="text" name="nome" id="" value=<?php echo $nome?> required >
-        <label for="">endereco</label>                                        
+        <label for="">Endereco</label>                                        
         <input type="text" name="endereco" id="" value=<?php echo $endereco ?> required >
-        <label for="">idade</label>
+        <label for="">Idade</label>
         <input type="text" name="idade" id="" value=<?php echo $idade ?> required >
-        <label for="">data</label>
+        <label for="">Data de Nascimento</label>
         <input type="text" name="datanascimento" id="" value=<?php echo $datanascimento ?> required >
 
-        <label for="">matricula</label>
+        <label for="">Matricula</label>
         <input type="text" name="matricula" id="" value=<?php echo $matricula ?> required >
       
         <input type="hidden" name="id" id="" value=<?php echo $id ?> >
         
         <input type="submit" name="update" value="Alterar">
   </form>
+</div>
+        
 </body>
 </html>
